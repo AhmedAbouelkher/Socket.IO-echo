4f16f2081fe1addea30fc4b38be192e3d901eed6
AhmedAbouelkher <am303737@gmail.com>
2021-04-27 14:15:20 +0200

    initial

---
+++ android/app/src/main/kotlin/com/example/casting_chat_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.casting_chat_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:casting_chat_2/pusher.dart';
+import 'package:casting_chat_2/socketio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _current = 'socketio';
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: Colors.white,
+        middle: CupertinoSegmentedControl(
+          children: {
+            'socketio': Text(
+              '  socket.io  ',
+              style: TextStyle(fontSize: 14),
+            ),
+            'pusher': Text(
+              'pusher',
+              style: TextStyle(fontSize: 14),
+            ),
+          },
+          onValueChanged: (value) {
+            setState(() {
+              _current = value;
+            });
+          },
+          groupValue: _current,
+        ),
+      ),
+      child: _current == 'socketio' ? SocketioPage() : PusherPage(),
+    );
+  }
+}
